327fac015766b57fd2fc03ebc286954fc4a372c0
Clément Martin <clement@livemon.com>
2019-05-15 11:10:49 +0200

    contact now includes links

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 import 'styles/Contact/contact.scss'
 
+const EMAIL = 'martinclementjules@gmail.com'
+const PHONE = {
+  world: '+33669255071',
+  fr: '06 69 25 50 71',
+}
+const LINKEDIN = {
+  url: 'https://www.linkedin.com/in/cl%C3%A9ment-martin-46024b138/',
+  id: 'clément-martin-46024b138',
+}
+
 const Contact = () => (
   <div className="contact-wrapper">
     <a
@@ -19,23 +29,45 @@ const Contact = () => (
       <span className="white">{'{ '}</span>
     </span>
     <span className="mobile-block">
-      <span className="green">email</span>
+      <span className="green">
+        <a href={`mailto:${EMAIL}`} title="Adresse e-mail">
+          email
+        </a>
+      </span>
       <span className="blue">:&nbsp;'</span>
-      <span className="white">martinclementjules@gmail.com</span>
+      <span className="white">
+        <a href={`mailto:${EMAIL}`} title="Adresse e-mail">
+          {EMAIL}
+        </a>
+      </span>
       <span className="blue">'</span>
       <span className="white">,&nbsp;</span>
     </span>
     <span className="mobile-block">
-      <span className="green">{`tel`}</span>
+      <span className="green">
+        <a href={`tel:${EMAIL}`} title="Téléphone">{`tel`}</a>
+      </span>
       <span className="blue">:&nbsp;'</span>
-      <span className="white">06 69 25 50 71</span>
+      <span className="white">
+        <a href={`tel:${PHONE.world}`} title="Téléphone">
+          {PHONE.fr}
+        </a>
+      </span>
       <span className="blue">'</span>
       <span className="white">,&nbsp;</span>
     </span>
     <span className="mobile-block">
-      <span className="green">linkedin</span>
+      <span className="green">
+        <a href={LINKEDIN.url} title="Linkedin Clément Martin" target="_blank">
+          linkedin
+        </a>
+      </span>
       <span className="blue">{`: '`}</span>
-      <span className="white">clément-martin-46024b138</span>
+      <span className="white">
+        <a href={LINKEDIN.url} title="Linkedin Clément Martin" target="_blank">
+          {LINKEDIN.id}
+        </a>
+      </span>
       <span className="blue">'&nbsp;</span>
     </span>
     <span className="white">{`}`}</span>
