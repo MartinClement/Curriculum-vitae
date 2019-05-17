18d71b3b8ecd0387f7b136b67450ffb68e9947b6
Clément Martin <clement@livemon.com>
2019-05-17 16:47:50 +0200

    circleci tests

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Typing } from 'react-typing'
+import nanoid from 'nanoid'
 
 import 'styles/Header/header.scss'
 
 const Header = () => (
   <div className="header-wrapper">
     {/* Should not be displayed on mobile*/}
-    <img className="me" src={require('assets/images/logos/trigerred.jpg')} />
+    <img className="me" src={`https://robohash.org/clement-martin${nanoid()}`} />
     <div className="header-title">
       <Typing delay={1000} keyDelay={50} className="header-typing">
         <span className="green">{`ClémentMartin`}</span>
